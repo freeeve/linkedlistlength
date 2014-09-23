f2165d03b3d679065253b294c29a04b98e70e86f
Wes Freeman <wes@skeweredrook.com>
2014-09-23 02:27:50 -0400

    init commit

---
+++ src/main/java/com/skeweredrook/linkedlistlength/LinkedListLength.java
@@ -0,0 +1,55 @@
+package com.skeweredrook.linkedlistlength;
+
+import org.neo4j.graphdb.*;
+import org.neo4j.graphdb.traversal.TraversalDescription;
+import org.neo4j.graphdb.traversal.Traverser;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import java.nio.charset.Charset;
+import java.util.Comparator;
+
+@Path( "/" )
+public class LinkedListLength
+{
+	private static final DynamicRelationshipType COMMENTS = DynamicRelationshipType.withName( "COMMENTS" );
+
+	@GET
+	@Produces( MediaType.TEXT_PLAIN )
+	@Path( "/{nodeId}" )
+	public Response length(@Context GraphDatabaseService db, @PathParam( "nodeId" ) long nodeId )
+	{
+		int length = 0;
+		Transaction tx = db.beginTx();
+		try {
+			Node startNode = db.getNodeById( nodeId );
+			TraversalDescription traversal = db.traversalDescription();
+			Traverser traverse = traversal
+				.depthFirst()
+				.relationships( COMMENTS, Direction.OUTGOING )
+				.sort( new Comparator<org.neo4j.graphdb.Path>()
+				{
+					@Override
+					public int compare( org.neo4j.graphdb.Path o1, org.neo4j.graphdb.Path o2 )
+					{
+						return Integer.valueOf( o2.length() ).compareTo( o1 .length() );
+					}
+				} )
+				.traverse( startNode );
+
+			length = traverse.iterator().next().length();
+
+			tx.failure();
+		} catch(Exception ex) {
+			tx.failure();
+		}
+		return Response.status( Status.OK ).entity(
+			("" + length).getBytes(Charset.forName("UTF-8"))).build();
+	}
+}
